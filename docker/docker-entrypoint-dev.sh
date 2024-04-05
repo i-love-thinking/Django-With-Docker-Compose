85c39555df63dce30e74eda9ac2d521216467422
morpheus.lee <o988184254@gmail.com>
2024-04-05 17:12:53 +0800

    Remove unnecessary files and update Docker configuration - change uwsgi to gunicorn - django upgrade to 5.0.3 - remove mysite, it create by web-app.

--- docker/docker-entrypoint-dev.sh
+++ docker/docker-entrypoint-dev.sh
@@ -10,4 +10,4 @@ python3 manage.py createsuperuser --noinput || echo "Super user already created"
 echo "--------------- Collect Static"
 python3 manage.py collectstatic --noinput
 echo "--------------- Running server"
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver
